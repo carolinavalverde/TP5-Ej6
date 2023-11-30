f39da82fae87fbbeec01a6b63daa8301f42a5c3e
Caro Valverde <carolina.valverde255@gmail.com>
2023-11-30 20:52:16 -0300

    agrego codigo js

--- js/Ej6.js
+++ js/Ej6.js
@@ -1,1 +1,57 @@
+document.addEventListener("DOMContentLoaded", function () {
+  let temporizador;
+  let tiempoInicial = 60;
+  let tiempoRestante = tiempoInicial;
+  let corriendo = false;
 
+  function iniciarTemporizador() {
+    if (!corriendo) {
+      corriendo = true;
+      temporizador = setInterval(function () {
+        if (tiempoRestante > 0) {
+          tiempoRestante--;
+          mostrarTiempo();
+        } else {
+          pausarTemporizador();
+        }
+      }, 1000);
+    }
+  }
+
+  function pausarTemporizador() {
+    corriendo = false;
+    clearInterval(temporizador);
+  }
+
+  function resetearTemporizador() {
+    corriendo = false;
+    clearInterval(temporizador);
+    tiempoRestante = tiempoInicial;
+    mostrarTiempo();
+  }
+
+  function mostrarTiempo() {
+    const segundos = tiempoRestante % 60;
+    const minutos = Math.floor(tiempoRestante / 60) % 60;
+    const horas = Math.floor(tiempoRestante / 3600);
+
+    document.getElementById("temporizador").innerText = `${horas
+      .toString()
+      .padStart(2, "0")}:${minutos.toString().padStart(2, "0")}:${segundos
+      .toString()
+      .padStart(2, "0")}`;
+  }
+
+  document.getElementById("iniciar").addEventListener("click", function () {
+    tiempoInicial = parseInt(document.getElementById("tiempo").value, 10);
+    resetearTemporizador();
+    iniciarTemporizador();
+  });
+
+  document
+    .getElementById("pausar")
+    .addEventListener("click", pausarTemporizador);
+  document
+    .getElementById("resetear")
+    .addEventListener("click", resetearTemporizador);
+});
